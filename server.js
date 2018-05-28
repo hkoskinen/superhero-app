ebc545a04a4a4a12d12a55e452624fe1dd864d1c
Henri Koskinen <hkoskinen@protonmail.com>
2018-05-28 21:43:41 +0300

    Play with the code

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const SuperHero = require('./models/superhero');
+
+mongoose.connect('mongodb://dev:dev@localhost/superhero');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -12,12 +16,28 @@ app.get('/', (req, res) => {
 });
 
 app.post('/handle', (req, res) => {
+/*
+  SuperHero.create({
+    name: req.body.name,
+    superhero: req.body.superhero
+  }).then((user) => {
+    return res.status(201).json(user); // returns the created resource
+    //res.send({name: req.body.name, superhero: req.body.superhero});
+  }, () => {
+    res.status(500).send({message: 'Error saving the superhero to database'})
+  });
+*/
+
   res.render('index', {
     formData: {
       name: req.body.name || '',
       superhero: req.body.superhero || ''
     }
   });
+});
+
+app.get('/heroes', (req, res) => {
+  SuperHero.find().then(data => res.status(200).json(data));
 });
 
 const PORT = process.env.PORT || 3000;
